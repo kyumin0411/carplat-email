ca0d4a5bad8fa869882c9f956397627d8be8a9f6
kyumin0411 <rbalsgood@gmail.com>
2021-07-26 18:52:56 +0900

    email.controller.ts 수정

--- app/dto/orders.dto.ts
+++ app/dto/orders.dto.ts
@@ -1,5 +1,100 @@
 import { ApiProperty } from '@nestjs/swagger';
 
+export class OrdersDTO {
+  @ApiProperty({ description: '예약 uuid' })
+  id: string;
+  @ApiProperty({
+    description:
+      '예약 상태 [ 1 차량예약 | 2 차량대여 | 3 예약취소 | 4 반납지연 | 5 이용완료 ]',
+  })
+  status: number;
+  @ApiProperty({ description: '대여기간시작일' })
+  startDate: string;
+  @ApiProperty({ description: '대여종료일' })
+  endDate: string;
+  @ApiProperty({ description: '차량번호' })
+  carNumber: string;
+  @ApiProperty({ description: '차량회사' })
+  carCorp: string;
+  @ApiProperty({ description: '차량이름' })
+  carName: string;
+  @ApiProperty({ description: '차량이미지 URL' })
+  carImageUrl: string;
+  @ApiProperty({ description: '목적 [ 외근 | 출장 | 사내행사 | 기타 ]' })
+  purpose: string;
+  @ApiProperty({ description: '목적 상세' })
+  purposeDetail: string;
+  @ApiProperty({ description: '사원이름' })
+  employeeName: string;
+  @ApiProperty({ description: '사원전화번호' })
+  employeePhoneNumber: string;
+  @ApiProperty({ description: '사원 부서명' })
+  departmentName: string;
+  @ApiProperty({ description: '사원번호' })
+  employeeNumber: string;
+  @ApiProperty({ description: '예약 생성일' })
+  insertDate: string;
+  @ApiProperty({ description: '사용 시작일' })
+  usageStartDate: string;
+  @ApiProperty({ description: '사용 종료일' })
+  usageEndDate: string;
+  @ApiProperty({ description: '이용시간  (단위 시간)' })
+  usagePeriod: number;
+  @ApiProperty({ description: 'bill 존재 여부' })
+  existBills: boolean;
+
+  constructor(
+    id?,
+    status?,
+    startDate?,
+    endDate?,
+    carNumber?,
+    carCorp?,
+    carName?,
+    carImageUrl?,
+    purpose?,
+    purposeDetail?,
+    employeeName?,
+    employeePhoneNumber?,
+    departmentName?,
+    employeeNumber?,
+    insertDate?,
+    usageStartDate?,
+    usageEndDate?,
+    usagePeriod?,
+    existBills?,
+  ) {
+    this.id = id;
+    this.status = status;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.carNumber = carNumber;
+    this.carCorp = carCorp;
+    this.carName = carName;
+    this.carImageUrl = carImageUrl;
+    this.purpose = purpose;
+    this.purposeDetail = purposeDetail;
+    this.employeeName = employeeName;
+    this.employeePhoneNumber = employeePhoneNumber;
+    this.departmentName = departmentName;
+    this.employeeNumber = employeeNumber;
+    this.insertDate = insertDate;
+    this.usageStartDate = usageStartDate;
+    this.usageEndDate = usageEndDate;
+    this.usagePeriod = usagePeriod;
+    this.existBills = existBills;
+  }
+}
+export class GetOrdersMetaDTO {
+  @ApiProperty({ description: '전체 카운트' })
+  totalCount: number;
+}
+export class GetOrdersResultDTO {
+  @ApiProperty({ description: '', type: GetOrdersMetaDTO })
+  meta: GetOrdersMetaDTO;
+  @ApiProperty({ type: OrdersDTO, isArray: true })
+  orders: OrdersDTO[];
+}
 export class SupplierDTO {
   @ApiProperty({ description: '업체 상호명' })
   companyName: string;
